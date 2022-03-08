53b7d335283a5f12ce50490a870ee64bf1586e4f
Andrew <Manager@ImagineIT.com.au>
2022-03-08 17:59:09 +1000

    first polling version to detect state

--- nexus21/tvlift.js
+++ nexus21/tvlift.js
@@ -27,6 +27,8 @@ module.exports = function(RED) {
 
         var node = this;
 
+        node.timer = null;
+
         if (!this.ip || (this.ip.length < 10)) {
             node.status({
                 fill:   "red",
@@ -37,9 +39,12 @@ module.exports = function(RED) {
         }
 
         var setStatus = function(json) {
+            const oldVertical = node.VERTICAL;
+            const oldHorizontal = node.HORIZONTAL;
             node.VERTICAL = json['VERTICAL'] || 'Unknown';
             node.HORIZONTAL = json['HORIZONTAL'] || 'Unknown';
-            node.working = node.VERTICAL != 'Unknown';
+            node.working = node.VERTICAL !== 'Unknown';
+            const changed = (oldVertical !== node.VERTICAL) || (oldHorizontal !== node.HORIZONTAL);
 
             if (!node.working) {
                 node.error(json);
@@ -50,11 +55,18 @@ module.exports = function(RED) {
                 });
                 return;
             }
-            node.status({
-                fill:   'green',
-                shape:  'dot',
-                text:   node.VERTICAL + ( node.HORIZONTAL != 'NA' ? '/' + node.HORIZONTAL : '')
-            });
+            if (changed) {
+                // todo: add decoded error payload if required
+                const msg = {
+                    payload: Object.assign({}, json)
+                };
+                node.send(msg); // pass on the status update to any listening nodes
+                node.status({
+                    fill: 'green',
+                    shape: 'dot',
+                    text: node.VERTICAL + (node.HORIZONTAL != 'NA' ? '/' + node.HORIZONTAL : '')
+                });
+            }
         };
 
         setStatus({});  // start in an unknown state
@@ -62,41 +74,14 @@ module.exports = function(RED) {
         //
         // check connectivity/update current status
         //
-        axios.get(`http://${node.ip}/api/status`)
-            .then(response => {
-                setStatus(response.data);
-                // don't go active here, wait to see if socket connects, it will maintain the correct active state
-            })
-            .catch(error => {
-                node.error(error.message || 'Unknown Error');
-                node.status({
-                    fill:   'red',
-                    shape:  'dot',
-                    text:   error.message || 'Unknown Error'
-                });
-            });
 
-        //const nodeId = node.id.replace(/\./g, '_');
-        // currentNodes && currentNodes.add(nodeId);
-
-        // send Commands to the TV LIFT
-
-        node.on('input', function(msg) {
-            node.log('tv lift input: ' + JSON.stringify(msg.payload, null, 2));
-
-            if (!msg.payload || (['UP', 'DOWN', 'MEM1', 'MEM2', 'MEM3'].indexOf(msg.payload) < 0)) {
-                node.error('invalid payload, must be one of "UP", "DOWN", "MEM1", "MEM2" or "MEM3"');
-                node.status({
-                    fill:   'red',
-                    shape:  'dot',
-                    text:   'invalid payload'
-                });
-                return;
-            }
-
-            axios.post(`http://${node.ip}/api/command`, { "COMMAND": msg.payload })
+        const pollStatus = function(callback) {
+            axios.get(`http://${node.ip}/api/status`)
                 .then(response => {
-                    node.log(msg.payload + ': ' + ((response.data && response.data.STATUS) || "?"));
+                    setStatus(response.data);
+                    if (callback) {
+                        callback(null);
+                    }
                 })
                 .catch(error => {
                     node.error(error.message || 'Unknown Error');
@@ -106,6 +91,67 @@ module.exports = function(RED) {
                         text:   error.message || 'Unknown Error'
                     });
                 });
+        };
+
+        pollStatus(function(err) {
+            if (err) { return; }
+            // todo: use a socket connection intead of polling
+            // todo: don't go active here, wait to see if socket connects, it will maintain the correct active state
+            node.timer = setInterval(function(){
+                pollStatus();
+            }, 5000);
+        });
+
+        //const nodeId = node.id.replace(/\./g, '_');
+        // currentNodes && currentNodes.add(nodeId);
+
+        // send Commands to the TV LIFT
+
+        node.on('input', function(msg, send, done) {
+            node.log('tv lift input: ' + JSON.stringify(msg.payload, null, 2));
+
+            if (!msg.payload || (['UP', 'DOWN', 'MEM1', 'MEM2', 'MEM3'].indexOf(msg.payload) < 0)) {
+                node.error('invalid payload, must be one of "UP", "DOWN", "MEM1", "MEM2" or "MEM3"');
+                node.status({
+                    fill:   'red',
+                    shape:  'dot',
+                    text:   'invalid payload'
+                });
+                if (done) {
+                    done();
+                }
+                return;
+            }
+
+            // fallback to using `node.send`
+            // send = send || function() { node.send.apply(node,arguments) }
+
+            axios.post(`http://${node.ip}/api/command`, { "COMMAND": msg.payload })
+                .then(response => {
+                    node.log(msg.payload + ': ' + ((response.data && response.data.STATUS) || "?"));
+                    if (done) {
+                        done();
+                    }
+                })
+                .catch(error => {
+                    node.status({
+                        fill:   'red',
+                        shape:  'dot',
+                        text:   error.message || 'Unknown Error'
+                    });
+                    if (done) {
+                        done(error);
+                    } else {
+                        node.error(error.message || 'Unknown Error');
+                    }
+                });
+        });
+
+        node.on('close', function() {
+            if (node.timer) {
+                clearInterval(node.timer);
+                node.timer = null;
+            }
         });
 
     }
